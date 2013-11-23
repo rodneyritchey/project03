de2135813e2b3e562569cfc88df9e9e04749f136
Rodney Ritchey <rodneyritchey@gmail.com>
2013-11-23 17:57:05 -0500

    Comment Display Commit
    Fixed comment display to pull dynamic data from database

--- js/ajax/comment_display.js
+++ js/ajax/comment_display.js
@@ -1,14 +1,27 @@
-//Member progress
 $(document).ready(function() {
-				$.ajax({
-					data: 'null',
-					type: 'get',
+	$('body').on('click', 'a', function(e, comment_id)
+		{
+			e.preventDefault();
+
+			var comment_id = ($(this).text());
+			//var comment_id = ($(this).attr('id'));
+
+			//$('div.comments_wrapper').toggleClass('comments_wrapper comments_wrapper_on');
+			//$('#comments_wrapper').empty().html('<h4>Hi ' + comment + '</h4>');
+
+			$.ajax({
+					data: {comment_id: comment_id},
+					type: 'post',
 					url: 'scripts/comment_display.php',
 					success: function(responseData) {
 						$('#comments_wrapper').empty().html(responseData);
 					},
 					error: function(responseData) {
-						$('#comments_wrapper').empty().html(responseData);
+						console.log('the login_check.php ajax call failed');
 					}
 				});
-			});
+
+		});
+
+
+});
--- scripts/comment_display.php
+++ scripts/comment_display.php
@@ -3,15 +3,17 @@
 
 	require_once('db_connect.php');
 
-	$usr=$_COOKIE['usr'];
+	//$usr=$_COOKIE['usr'];
 
-	//echo $sesh_id;
+	$comment_id=$_POST['comment_id'];
 
-	$result = mysqli_query($con,"SELECT * FROM user WHERE usr = '".$usr."'");
+	//echo $comment_id;
+
+	$result = mysqli_query($con,"SELECT * FROM user WHERE tag = '".$comment_id."'");
 
 	
 while($row = mysqli_fetch_array($result))
   {
-   echo "<div class='user_says' id='user_says'><h4>" .$usr. " says</h4></div><div class='comment id='comment'>&quot;" .$row['comment']. "&quot;</div>";  
+   echo "<div class='user_says' id='user_says'><h4>" .$row['usr']. " says</h4></div><div class='comment id='comment'>&quot;" .$row['comment']. "&quot;</div>";  
   }
  ?> 

30ce306e46feecdc464dcbd9148add664c517595
Rodney Ritchey <rodneyritchey@gmail.com>
2013-11-26 14:51:20 -0500

    Fixed success.php
    Fixed grammar for tag success

--- scripts/tag_success.php
+++ scripts/tag_success.php
@@ -9,14 +9,21 @@
 
 	$result = mysqli_query($con,"SELECT * FROM user WHERE usr = '".$usr."'");
 
-	
-while($row = mysqli_fetch_array($result))
+
+  while($row = mysqli_fetch_array($result))
   {
    $pic = mysqli_query($con,"SELECT * FROM locations WHERE primary_key = '".$row['progress']."'");
-	   while($row = mysqli_fetch_array($pic))
-	  {
-	   echo "<h3>Congratulations!</h3><h3>You tagged " .$row['name']. "!</h3>";
-	      
-	  };  
+	   while($row = mysqli_fetch_array($pic)){
+	   	if($row['primary_key'] == 1) {
+	   		echo "<h3>Congratulations!</h3><h3>You tagged the " .$row['name']. "!</h3>";
+	  	}
+	  	else if($row['primary_key'] == 3 || $row['primary_key'] == 4) 
+	  	{
+	  		echo "<h3>Congratulations!</h3><h3>You tagged " .$row['name']. "!</h3>";
+	  	}
+	  	else {
+	  		echo "<h3><h3>Congratulations!</h3><h3>You tagged the " .$row['name']. "!</h3>";
+	  	}
+	};
   }
  ?> 

3c8d25be1bc8ee4701606843195d6a13bd2eccc5
Rodney Ritchey <rodneyritchey@gmail.com>
2013-11-26 16:28:51 -0500

    Update Comment Display
    Comments now correspond to location

--- scripts/comment_display.php
+++ scripts/comment_display.php
@@ -6,14 +6,33 @@
 	//$usr=$_COOKIE['usr'];
 
 	$comment_id=$_POST['comment_id'];
+	$usr=$_COOKIE['usr'];
 
 	//echo $comment_id;
 
 	$result = mysqli_query($con,"SELECT * FROM user WHERE usr = '".$comment_id."'");
-
-	
+	$progress = mysqli_query($con,"SELECT progress FROM user WHERE usr = '".$usr."'");
+while($row = mysqli_fetch_array($progress)) {
+	$prog = $row['progress'];
 while($row = mysqli_fetch_array($result))
+
   {
-   echo "<div class='user_says' id='user_says'><h4>" .$row['usr']. " says</h4></div><div class='comment id='comment'>&quot;" .$row['comment']. "&quot;</div>";  
+  	if ($prog == 1) {
+   		echo "<div class='user_says' id='user_says'><h4>" .$row['usr']. " says</h4></div><div class='comment id='comment'>&quot;" .$row['comment01']. "&quot;</div>";  
+  	}
+  	else if ($prog == 2) {
+  		echo "<div class='user_says' id='user_says'><h4>" .$row['usr']. " says</h4></div><div class='comment id='comment'>&quot;" .$row['comment02']. "&quot;</div>";
+  	}
+  	else if ($prog == 3) {
+  		echo "<div class='user_says' id='user_says'><h4>" .$row['usr']. " says</h4></div><div class='comment id='comment'>&quot;" .$row['comment03']. "&quot;</div>";
+  	}
+  	else if ($prog == 4) {
+  		echo "<div class='user_says' id='user_says'><h4>" .$row['usr']. " says</h4></div><div class='comment id='comment'>&quot;" .$row['comment04']. "&quot;</div>";
+  	}
+  	else if ($prog == 5) {
+  		echo "<div class='user_says' id='user_says'><h4>" .$row['usr']. " says</h4></div><div class='comment id='comment'>&quot;" .$row['comment05']. "&quot;</div>";
+  	}
+  	else {}
   }
+}
  ?> 

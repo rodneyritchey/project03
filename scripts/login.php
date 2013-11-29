2acb0d330f60fd3facf4f49c5c455c9774f0e1e5
Rodney Ritchey <rodneyritchey@gmail.com>
2013-11-28 20:13:59 -0500

    Login/Register Update
    Fixed login.php

--- scripts/login.php
+++ scripts/login.php
@@ -1,33 +1,25 @@
 <?php 
 
-//Model function that adds a user by creating a new auto incremented record
 
 require_once ('db_connect.php');
 
 $username = $_POST["username"];
 $email = $_POST["email"];
-//Sets Session with vote_id
-//require_once ('set_session.php');
 
-$results = mysqli_query($con,"SELECT * FROM user WHERE usr = '".$username."'");
-if($results) {
+$results = mysqli_query($con,"SELECT * FROM user WHERE usr = '" .$username. "'");
+$count = mysqli_num_rows($results);
+//echo $count;
+if ($count == 1) {
 
-        //Sets cookie with id user and $vote_id as value
- require_once ('set_cookie.php');
-
- $sql="INSERT INTO user (usr, email, progress) VALUES ('$username','$email','1')";
-
- if (!mysqli_query($con,$sql))
-   {
-   die('Error: ' . mysqli_error($con));
-   }
- //echo "1 record added";
- //mysqli_close($con);
-
- echo ('members.php');
+	require_once ('set_cookie.php');
+	echo ('members.php');
 }
 else {
- require_once ('set_cookie.php');
- echo ('members.php');
+	
+	require_once ('set_cookie.php');
+
+ 	$sql = mysqli_query($con,"INSERT INTO user (usr, email, progress) VALUES ('$username','$email','1')");
+ 	echo ('members.php');
 }
+
 ?>

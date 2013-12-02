dfc280680d0f15b4eb4385654a699968913c9cd4
Rodney Ritchey <rodneyritchey@gmail.com>
2013-12-02 16:47:19 -0500

    Redirect Update
    Fixed redirect to include finished status.

--- scripts/index_redirect.php
+++ scripts/index_redirect.php
@@ -1,8 +1,23 @@
 <?php
 session_start();
 
+require_once('db_connect.php');
+
+	$usr=$_COOKIE['usr'];
+
+	//echo $sesh_id;
+
+	$result = mysqli_query($con,"SELECT * FROM user WHERE usr = '".$usr."'");
+
 if (isset($_COOKIE['usr'])) {
-  echo ('members.php');
+	while($row = mysqli_fetch_array($result)) {
+	  	if($row['progress'] == 6) {
+	  		echo "finished.php";
+	  	}
+	  	else {
+  			echo ('members.php');
+		}
+	}
 }
 else {
   echo ('login_index.php');

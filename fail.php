2d3067b4deb22486d26dede4ca91fafff80b8a96
Rodney Ritchey <rodneyritchey@gmail.com>
2013-12-03 13:03:04 -0500

    Google Analytics Update
    Moved Google Analytics call around

--- fail.php
+++ fail.php
@@ -1,5 +1,15 @@
 <!DOCTYPE html>
 <html>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46170663-1', 'rodneyritchey.com');
+  ga('send', 'pageview');
+
+</script>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,16 +21,7 @@
 <script>            
    $(setTimeout("window.location.replace('members.php')", 3500));
 </script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-46170663-1', 'rodneyritchey.com');
-  ga('send', 'pageview');
-
-</script>
 </head>
 
 <body>

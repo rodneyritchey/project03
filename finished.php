1e7cea9e0fb2d016bce904e9dcf760e2af49b3e0
Rodney Ritchey <rodneyritchey@gmail.com>
2013-12-03 14:54:23 -0500

    Styles and Layout Update
    Fixed finished.php display and survey link placement.

--- finished.php
+++ finished.php
@@ -8,16 +8,7 @@
 <link rel="stylesheet" href="css/styles.css" />
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="js/ajax/member_check.js"></script>
-<script src="js/tagit.js"></script><script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-46170663-1', 'rodneyritchey.com');
-  ga('send', 'pageview');
-
-</script>
+<script src="js/tagit.js"></script>
 </head>
 
 <body>
@@ -36,12 +27,13 @@
 		
 		<div class="btm_wrapper" id="btm_wrapper">
 			<div class="loc_text" id="loc_text"></div>
-			<div class="finished_wrapper">
-				<img src="img/check_mark_green.png" />
-			</div>
 			<div class="survey">
 				<a href="https://www.surveymonkey.com/s/RPWX85B">Click here to take survey!</a>
 			</div>
+			<div class="finished_wrapper">
+				<img src="img/check_mark_green.png" />
+			</div>
+			
 		</div>
 	</div>
 </body>
